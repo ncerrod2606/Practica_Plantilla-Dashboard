2734b99f685779a6fb569e990227bff29bb8788b
ncerrod2606 <ncerrod2606@ieszaidinvergeles.org>
2025-10-22 22:07:51 +0200

    Modificación de archivos

--- dashboard/routes/web.php
+++ dashboard/routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MainController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [MainController::class, 'index'])->name('dashboard');
